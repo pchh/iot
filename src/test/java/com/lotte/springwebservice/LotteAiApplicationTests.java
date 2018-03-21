4849f20b996a34f8592165e72388ff2bf1224d39
BokDaHoon <BokDaHoon>
2018-03-21 16:57:15 +0900

    2018-03-21 init server

---
+++ src/test/java/com/lotte/springwebservice/LotteAiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lotte.springwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LotteAiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
