c1fe56f3b5321df8370acb624a4f09a41662ed66
devwon <wonhyerin96@gmail.com>
2018-08-06 10:54:23 +0900

    hyerin branch에서 hello.vm 수정

--- src/main/java/com/lotte/coffee/controller/CoffeeController.java
+++ src/main/java/com/lotte/coffee/controller/CoffeeController.java
@@ -170,9 +170,38 @@ public class CoffeeController {
 	public ModelAndView insertBoard(@RequestBody HashMap<String, String> param) {
  		ModelAndView mav = new ModelAndView();
 		int insertYn = service.insertBoard(param);
-		mav.setViewName("master");
+		mav.setViewName("insertBoard");
 		return mav;
+		//response.setContentType("text/html;charset=utf-8");
+//		PrintWriter out = response.getWriter();
+//		@Override
+//		public void profileUpload(String email, MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws Exception {
+//			response.setContentType("text/html;charset=utf-8");
+//			PrintWriter out = response.getWriter();
+//			// 업로드할 폴더 경로
+//			String realFolder = request.getSession().getServletContext().getRealPath("profileUpload");
+//			UUID uuid = UUID.randomUUID();
+//
+//			// 업로드할 파일 이름
+//			String org_filename = file.getOriginalFilename();
+//			String str_filename = uuid.toString() + org_filename;
+//
+//			System.out.println("원본 파일명 : " + org_filename);
+//			System.out.println("저장할 파일명 : " + str_filename);
+//
+//			String filepath = realFolder + "\\" + email + "\\" + str_filename;
+//			System.out.println("파일경로 : " + filepath);
+//
+//			File f = new File(filepath);
+//			if (!f.exists()) {
+//				f.mkdirs();
+//			}
+//			file.transferTo(f);
+//			out.println("profileUpload/"+email+"/"+str_filename);
+//			out.close();
+//		}
 	}	
+	
 	
 	@RequestMapping(value="/updateBoard", method=RequestMethod.POST)
 	public ModelAndView updateBoard(@RequestBody HashMap<String, String> param) {
@@ -189,7 +218,6 @@ public class CoffeeController {
 		mav.setViewName("master");
 		return mav;
 	}
-
 
 	@ResponseBody
 	@RequestMapping(value="/trace", method=RequestMethod.GET)
